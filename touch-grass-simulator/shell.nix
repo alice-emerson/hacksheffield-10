dd538b7f807a8ff8b6854128a5d4f116b6de5389
CelestialShark <lizziegibbs@proton.me>
2025-11-29 18:21:02 +0000

    Fixed shell.nix for monogame

--- touch-grass-simulator/shell.nix
+++ touch-grass-simulator/shell.nix
@@ -1,7 +1,16 @@
 { pkgs ? import <nixpkgs> {} }:
 	pkgs.mkShell {
 		nativeBuildInputs = with pkgs.buildPackages; [
-			dotnet-sdk_9
+					dotnet-sdk_9
       		dotnetPackages.Nuget
+					mono
+			];
+
+			LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
+					freetype
+					libGL
+					pulseaudio
+					xorg.libX11
+					xorg.libXrandr
 			];
 }
